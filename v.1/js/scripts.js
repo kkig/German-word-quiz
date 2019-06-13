a4cb1423baac15183fc5260469c46a9bb8670207
Kaolu <kigoshi.kanazawa@gmail.com>
2019-06-13 23:58:51 +0200

    Add files via upload

---
+++ v.1/js/scripts.js
@@ -0,0 +1,10 @@
+let word = document.querySelector('.word');
+const buttonOne = document.querySelector('#buttonOne');
+
+
+function wordDisplay() {
+  word.innerHTML = 'Sample';
+  buttonOne.innerHTML = 'ButtonOne';
+}
+
+wordDisplay();
