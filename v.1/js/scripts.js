89a57809e82ac0c7b7d305b64919235ecd627302
Kaolu <kigoshi.kanazawa@gmail.com>
2019-06-14 23:40:56 +0200

    Add files via upload

--- v.1/js/scripts.js
+++ v.1/js/scripts.js
@@ -1,10 +1,45 @@
 let word = document.querySelector('.word');
-const buttonOne = document.querySelector('#buttonOne');
+const choiceOne = document.querySelector('#choiceOne');
+const choiceTwo = document.querySelector('#choiceTwo');
+const choiceThree = document.querySelector('#choiceThree');
+let x;
+let y;
+let z;
 
+function setRandom() {
+  let isDuplicate = true;
 
-function wordDisplay() {
-  word.innerHTML = 'Sample';
-  buttonOne.innerHTML = 'ButtonOne';
+  function pickWord() {
+    x = Math.floor(Math.random() * library.length);
+    y = Math.floor(Math.random() * library.length);
+    z = Math.floor(Math.random() * library.length);
+    if (x != y && y != z && x != z) {
+      isDuplicate = false;
+    }
+
+  }
+
+  while (isDuplicate) {
+    pickWord();
+  }
 }
 
-wordDisplay();
+function changeButton() {
+  choiceOne.classList.remove('btn-outline-primary');
+  choiceOne.classList.add('btn-success');
+  choiceTwo.classList.remove('btn-outline-primary');
+  choiceTwo.classList.add('btn-danger');
+  choiceThree.classList.remove('btn-outline-primary');
+  choiceThree.classList.add('btn-danger');
+}
+
+
+choiceOne.addEventListener('click', changeButton);
+choiceTwo.addEventListener('click', changeButton);
+choiceThree.addEventListener('click', changeButton);
+
+setRandom();
+word.innerHTML = library[x].word;
+choiceOne.innerHTML = library[x].translation;
+choiceTwo.innerHTML = library[y].translation;
+choiceThree.innerHTML = library[z].translation;
